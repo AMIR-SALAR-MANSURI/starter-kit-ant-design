e60c0477534b003b95ab317c1f6d6927c6ac8c0e
Morteza-Vahdati <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-06-21 15:00:26 +0330

    add react query and service base

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ConfigProvider, theme } from "antd";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ConfigProvider theme={theme}>
-        <App />
-      </ConfigProvider>
-    </BrowserRouter>
+    <QueryClientProvider>
+      <BrowserRouter>
+        <ConfigProvider theme={theme}>
+          <App />
+        </ConfigProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
